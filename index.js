2a1b64e34555094f9010c7190b0cf54029b61620
aksh <malviyaaksh@gmail.com>
2023-06-16 19:43:43 +0530

    modified:   index.js 	modified:   models/campground.js 	modified:   seeds/index.js 	modified:   views/campground/editCamp.ejs 	modified:   views/campground/index.ejs 	modified:   views/campground/newCamp.ejs 	modified:   views/campground/show.ejs 	modified:   views/home.ejs 	modified:   views/layouts/boilerplate.ejs 	modified:   views/partials/navbar.ejs 	AppError.js 	views/campground/geo-alt-fill.svg

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const ejsMate = require("ejs-mate")
 
+const AppError = require("./AppError")
 const Campground = require("./models/campground")
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -19,15 +21,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
     })
 const app = express()
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate) 
 app.set("view engine", 'ejs')
 app.set("views", path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride("_method"))
 // app.use((req,res,next)=>{
 //     console.log(req.path)
-//     next()
+//     next()      // these are kind of middleware which can be used to run security for specfic or all request
 // })
+
 app.get("/", (req, res) => {
     res.render('home')
 })
@@ -48,21 +51,28 @@ app.get("/campgrounds/:id/edit", async(req, res) => {
 })
 
 app.post("/campground", async (req, res) => {
-    const addCamp = new Campground(req.body)
+    const addCamp = new Campground(req.body.campground)
     await addCamp.save()
     res.redirect(`/campgrounds/${addCamp._id}`)
 })
 
 app.put("/campgrounds/:id", async(req,res)=>{
     const { id } = req.params
-    const update = await Campground.findByIdAndUpdate(id, {...req.body})
+    const update = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${id}`)
 })
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id", async (req, res , next) => { //adding next 
     const { id } = req.params
     const Camp = await Campground.findById(id)
+    res.send(Camp)
+    if ( !Camp ) {
+        // throw new AppError("Camp not found", 404 ) 
+        // we cannot do like this because it is async function instead we need to add next and then pass our error to it as:
+        return next(new AppError("Camp not found", 404 ))
+    }
     res.render('campground/show', { Camp })
+    console.log(Camp)
 })
 
 app.delete("/campgrounds/:id/", async(req,res)=>{
@@ -70,6 +80,17 @@ app.delete("/campgrounds/:id/", async(req,res)=>{
     await Campground.findByIdAndDelete(id)
     res.redirect("/campgrounds")
 })
+
+app.get("/secret" , (req,res)=>{
+    throw new AppError("Your are not admin",401)
+})
+
+app.use((err ,req,res,next)=>{
+    const { status= 500 , message = "Something Went Wrong"} = err
+    console.log("Errorrrrr!!!!!!!111")
+    res.status(status).send(message)
+}) // this is custom error handler which can handle error from all routes if thrown at time of running
+// but this cannot handle async request and they need to be defined at end
 app.listen(3000, () => {
     console.log("listening on port 3000")
 })
--- seeds/index.js
+++ seeds/index.js
@@ -18,10 +18,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)]
 const seedDB = async() =>{
     await campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
-        const random = Math.floor(Math.random()*1000)
+        const random = Math.floor(Math.random()*1000)        
+        const price = Math.floor(Math.random()*20)+10        
         const camp = new campground({
             location : `${cities[random].city}, ${cities[random].state}`,
-            title : `${sample(descriptors)} ${sample(places)}`
+            title : `${sample(descriptors)} ${sample(places)}` ,
+            image : "https://source.unsplash.com/random/?InTheWoods",
+            description :  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dolorem odio porro recusandae reiciendis cumque in magnam ipsam autem quo cupiditate sunt non fugit enim, ullam maxime dolor distinctio magni.",
+            price : price
         })
         camp.save()
     }
